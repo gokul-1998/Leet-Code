a3f5c473db401a854797cd5d54ef5f8e92f0c5b7
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-11-23 20:49:42 +0530

    better soln

--- 01431. Kids With the Greatest Number of Candies/solution.py
+++ 01431. Kids With the Greatest Number of Candies/solution.py
@@ -2,8 +2,14 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         max_all=max(candies)
+        op=[]
         for i in candies:
             op.append(i+extraCandies>=max_all)
-            
         return op
 
+
+## better solution
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_all = max(candies)
+        return [(candy + extraCandies) >= max_all for candy in candies]
